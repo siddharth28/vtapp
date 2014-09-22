f8f9533e144ea5e345c2deb3505b9481690ec57b
tanmay3011 <tanmay3011@gmail.com>
2014-09-22 18:40:50 +0530

    Initial Commit Add Basic SuperAdmin features 1. Add Login 2. Add all company list 3. Add SignOut 4. Add SuperAdmin account edit 5. Add create account

---
+++ db/migrate/20140922114917_add_user_ref_to_users.rb
@@ -0,0 +1,5 @@
+class AddUserRefToUsers < ActiveRecord::Migration
+  def change
+    add_reference :users, :company, index: true
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
+Role.delete_all
+Role.create!(name: 'super_admin')
+Role.create!(name: 'account_owner')
+Role.create!(name: 'account_admin')
+Role.create!(name: 'track_owner')
+Role.create!(name: 'task_reviewer')
+Role.create!(name: 'track_runner')
+User.create!(name: 'tanmay', email: 'tanmay@vinsol.com', password: 'vinsol123')
+User.first.add_role 'super_admin'
