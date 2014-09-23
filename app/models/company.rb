66aca7db7beab879da007eaf1f8185daa3a66400
tanmay3011 <tanmay3011@gmail.com>
2014-09-23 15:54:12 +0530

    Add method #owner_email to fetch account owners email

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,9 @@ class Company < ActiveRecord::Base
   has_many :users
   validates :name, presence: true, uniqueness: true
   accepts_nested_attributes_for :users
+
+  def owner_email(id)
+    User.owner(id).email
+  end
+
 end
