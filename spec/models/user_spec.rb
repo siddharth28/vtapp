b137772587e9b5874986273f3c4fa91fd84f6c03
tanmay3011 <tanmay3011@gmail.com>
2014-10-01 21:25:29 +0530

    [WIP] Rspecs for User Model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,107 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  before(:each) { @user = User.new(name: "Test User", password: "please123", email: 'user@example.com') }
+describe User do
+  let(:user) { build(:user) }
 
-  subject { @user }
+  describe 'associations' do
+    describe 'has_many association' do
+      it { should have_many(:mentees).with_foreign_key(:mentor_id).dependent(:nullify) }
+    end
 
-  it { should respond_to(:email) }
-  it { should respond_to(:name) }
+    describe 'belongs_to' do
+      it { should belong_to(:company) }
+      it { should belong_to(:mentor) }
+    end
+  end
 
-  it "#email returns a string" do
-    expect(@user.email).to match 'user@example.com'
+  describe 'validation' do
+
+    context 'mentor present' do
+      it do
+        user.mentor_id = 1
+        user.save
+        should validate_presence_of(:mentor)
+      end
+    end
+
+    context 'mentor not present' do
+      it { should_not validate_presence_of(:mentor) }
+    end
+  end
+
+  describe 'callbacks' do
+    let(:user) { build(:user, name: nil, email: nil, password: nil) }
+
+    describe 'before validation' do
+      it { expect { user.valid? }.to change{ user.password.nil? }.from(true).to(false) }
+    end
+
+    # describe 'after commit' do
+    #   after { run_callbacks(:commit) }
+    #   it { should_receive(:Mail) }
+    # end
+  end
+
+  describe 'attributes' do
+    it { expect(user).to respond_to(:email) }
+    it { expect(user).to respond_to(:company_id) }
+
+    describe 'readonly_attributes' do
+      it 'should not update readonly attribute email' do
+        user.update_attributes email: 'new_test@example.com'
+        expect(user.reload.email).to eql user.email
+      end
+
+      it 'should not update readonly attribute company_id' do
+        user.update_attributes company_id: 1
+        expect(user.reload.company_id).to eql user.company_id
+      end
+    end
+  end
+
+  describe 'instance methods' do
+    let(:user) { build(:user, name: nil, email: nil, password: nil) }
+    it { expect(user).not_to respond_to(:set_random_password) }
+    it { expect(user).not_to respond_to(:send_password_email) }
+    it { expect(user).to respond_to(:active_for_authentication?) }
+
+    describe '#active_for_authentication?' do
+      context 'user super_admin' do
+        it 'super_admin' do
+          user.add_role 'super_admin'
+          expect(user.active_for_authentication?).to eql(true)
+        end
+      end
+
+      context 'user not super_admin' do
+        let(:company) { build(:company, name: 'Vinsol', enabled: true ) }
+        let(:user) { company.users.build({ name: 'Vinsol', enabled: true } ) }
+        it 'enabled' do
+          user.add_role 'account_owner'
+          company.toggle!(:enabled)
+          expect(user.active_for_authentication?).to eql(false)
+        end
+      end
+    end
+
+    describe '#set_random_password' do
+      it { expect { user.send(:set_random_password) }.to change{ user.password.nil? }.from(true).to(false) }
+    end
+
+    # describe '#send_password_email' do
+    #   it { expect { user.send(:set_random_password) }.to change{ user.password.nil? }.from(true).to(false) }
+    # end
+  end
+
+  describe 'scope' do
+    describe 'owner' do
+      before do
+        user.add_role 'account_owner'
+        user.save
+      end
+      it 'account_owner' do
+        expect(User.owner.first.has_role? :account_owner).to eql(true)
+      end 
+    end
   end
 end
