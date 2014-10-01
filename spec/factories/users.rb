e23cbe5b9eea4a78e2e4c3bbe98f5f92cb7c9d79
tanmay3011 <tanmay3011@gmail.com>
2014-10-01 14:51:58 +0530

    Add factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,14 @@
 FactoryGirl.define do
   factory :user do
-    name "Test User"
-    email "test@example.com"
-    password "please123"
+    name 'Test User'
+    email 'test@example.com'
+    password 'please123'
+    enabled true
+    company_id 1
+  end
+
+  factory :company do
+    name 'Test Company'
+    enabled true
   end
 end
