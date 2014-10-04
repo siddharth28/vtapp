d160c3bfc8d5ae10e4e15dd4d158091e8688c364
tanmay3011 <tanmay3011@gmail.com>
2014-10-04 15:49:42 +0530

    Remove resource association

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,9 +2,8 @@ class Role < ActiveRecord::Base
   ## FIXED
   ## FIXME_NISH Verify if we need to specify join_table.
   has_and_belongs_to_many :users, join_table: :users_roles, dependent: :nullify
+  # FIXED
   ## FIXME_NISH do we required this resource association?
-  belongs_to :resource, polymorphic: true
-
   scope :with_name, ->(role_name) { where(name: role_name) }
 
   scopify
