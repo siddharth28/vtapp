4aa602bb7a909898834ceabd699b55c872e36942
tanmay3011 <tanmay3011@gmail.com>
2014-10-06 01:03:01 +0530

    Add association for owner

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,16 +1,16 @@
 class Company < ActiveRecord::Base
 
   has_many :users, dependent: :destroy
-  has_one :owner, -> { joins(:roles).where('roles.name' => 'account_owner') }, class_name: 'User'
+
+  ## FIXME_NISH Lets make an association of owner as discussed and also validate that there is always only one owner.
+  has_one :owner, -> { joins(:roles).merge(Role.with_name('account_owner')) }, class_name: 'User'
 
   accepts_nested_attributes_for :users
 
   validates :name, presence: true
   validates :name, uniqueness: true, allow_blank: true
 
-
+  # FIXED
   ## FIXME_NISH why this callback is a after_commit?
   ## FIXME_NISH Please move this callback to user as discussed.
-
-  ## FIXME_NISH Lets make an association of owner as discussed and also validate that there is always only one owner.
 end
