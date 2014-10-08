bce766125b4c3a898efa86c82abe05f41186f950
siddharth@vinsol.com <siddharth@vinsol.com>
2014-10-08 12:19:52 +0530

    fix seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,7 @@
 ## FIXED
 ## FIXME_NISH Don't delete users, roles etc. Instead use Role.find_or_create_by(name: 'super_admin')
-Role.find_or_create_by!(name: 'super_admin')
-Role.find_or_create_by!(name: 'account_owner')
-Role.find_or_create_by!(name: 'account_admin')
-Role.find_or_create_by!(name: 'track_owner')
-Role.find_or_create_by!(name: 'task_reviewer')
-Role.find_or_create_by!(name: 'track_runner')
-User.new(name: 'tanmay', email: 'tanmay@vinsol.com', password: 'vinsol123').save!(validate: false)
-User.find_by(email: 'tanmay@vinsol.com').add_role 'super_admin'
+roles = ['super_admin', 'account_owner', 'account_admin', 'track_owner', 'track_runner', 'task_reviewer']
+roles.each do |role|
+  Role.find_or_create_by!(name: role)
+end
+User.create!(name: 'tanmay', email: 'tanmay@vinsol.com', password: 'vinsol123').add_role 'super_admin'
