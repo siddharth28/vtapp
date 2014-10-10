038fe73b7d0acbeab9e17fa3bc100fbe32f28930
siddharth@vinsol.com <siddharth@vinsol.com>
2014-10-10 15:48:55 +0530

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,6 @@ roles = ['super_admin', 'account_owner', 'account_admin', 'track_owner', 'track_
 roles.each do |role|
   Role.find_or_create_by!(name: role)
 end
-super_admin = User.new(name: 'tanmay', email: 'tanmay@vinsol.com', password: 'vinsol123')
+super_admin = User.new(name: 'Super Admin', email: 'mailvtapp@gmail.com', password: 'superadmin123')
 super_admin.save(validate: false)
 super_admin.add_role(:super_admin)
