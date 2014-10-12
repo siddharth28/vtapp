08cf17c7301f67ad018003b9a7c699df84b37cc2
tanmay3011 <tanmay3011@gmail.com>
2014-10-13 02:06:21 +0530

    VT13 set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  mount Ckeditor::Engine => '/ckeditor'
   devise_scope :user do
     authenticated :user do
       root 'companies#index', as: :authenticated_root
+
+      resources :tracks do
+        get :autocomplete_user_name, :on => :collection
+      end
+
       #FIXED
       #FIXME Add routes for those actions only which are in use.
       resources :users, only: [:show, :edit]
