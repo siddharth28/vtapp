6a959925ae3e3f1fbbf1fa89464372f7cb92bc20
tanmay3011 <tanmay3011@gmail.com>
2014-10-13 00:43:24 +0530

    1. Remove all roles addition 2. Remove validates false

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,8 @@
 #FIXED
 #FIXME_AB: Don't commit and push unwanted files like tmp file and log files.
-# roles = ['super_admin', 'account_owner', 'account_admin', 'track_owner', 'track_runner', 'task_reviewer']
+#FIXED
 #FIXME_AB: I don't think we need to create all these roles. Because only super_admin would be a global role. And to assign any role, it is not necessary that role should be present in model. Apart from super_admin all roles would be resource specific. Kindly checkout rollify gem's doc.
-# roles.each do |role|
-  Role.find_or_create_by!(name: 'super_admin')
-# end
+Role.find_or_create_by!(name: 'super_admin')
 #FIXED
 #FIXME_AB: why validate set to be false
 super_admin = User.new(name: 'Super Admin', email: 'mailvtapp@gmail.com', password: 'superadmin123')
