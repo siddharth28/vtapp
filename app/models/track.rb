2ef2c61dc29350d8a134a9d5ec77b86bc09456a3
tanmay3011 <tanmay3011@gmail.com>
2014-10-14 17:09:27 +0530

    VT-13 Add track #owner functionality 1. Add owner_email and owner_name accessor

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,20 +1,25 @@
 class Track < ActiveRecord::Base
   belongs_to :company
-  validates :name, presence: true, uniqueness: { case_sensitive: false }, allow_blank: true
+
+  validates :name, uniqueness: { case_sensitive: false }, presence: true
   validates :references, presence: true
   validates :description, presence: true
   validates :instructions, presence: true
 
-  attr_accessor :track_owner
+  attr_accessor :owner_name, :owner_email
 
   after_create :assign_track_owner_role
 
+  def owner
+    company.users.with_role(:track_owner, self)
+  end
+
   private
     def assign_track_owner_role
-      if track_owner
-        company.users.find(track_owner.split(':')[1]).add_role(:track_owner, @track)
+      if owner_email
+        company.users.find_by(email: owner_email).add_role(:track_owner, @track)
       else
-        company.users.find(company.owner.first).add_role(:track_owner, @track)
+        company.owner.first.add_role(:track_owner, @track)
       end
     end
 end
