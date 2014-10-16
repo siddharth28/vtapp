02b2cace1885a5ac102bc999944e24391219e67b
tanmay3011 <tanmay3011@gmail.com>
2014-10-16 19:47:28 +0530

    VT-13 Update schema and routes after Link removal

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,26 @@ Rails.application.routes.draw do
         get 'users/new' => 'users#new', as: 'new_user'
         post 'users' => 'users#create'
       end
+
       root 'roles#home_page', as: :authenticated_root
+
       resources :users
-      resources :tracks, except: [:edit, :update, :destroy, :show] do
+      resources :tracks do
+        member do
+          patch :enable, to: :toggle_enabled
+          patch :disable, to: :toggle_enabled
+          get :assign_track_reviewer
+          post :assign_track_reviewer
+          get :remove_reviewer
+          delete :remove_reviewer
+        end
         get :autocomplete_user_name, on: :collection
       end
       resources :companies, except: [:edit, :update, :destroy] do
-        patch :enable, on: :member, to: :toggle_enabled
-        patch :disable, on: :member, to: :toggle_enabled
+        member do
+          patch :enable, to: :toggle_enabled
+          patch :disable, to: :toggle_enabled
+        end
       end
     end
 
