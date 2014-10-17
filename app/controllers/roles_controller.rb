df52bc37f2522589a2eb2f5e11251c85baaa5a74
siddharth@vinsol.com <siddharth@vinsol.com>
2014-10-17 17:44:22 +0530

    fix root path

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -2,7 +2,7 @@ class RolesController < ApplicationController
   def home_page
     if current_user.has_role? :super_admin
       redirect_to companies_path
-    elsif current_user.has_role? :account_owner
+    elsif current_user.has_role? :account_owner, :any
       redirect_to users_path
     end
   end
