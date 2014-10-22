58ea7a23c2d8468ba8f1d37bcf7d273dda219421
siddharth@vinsol.com <siddharth@vinsol.com>
2014-10-22 17:58:30 +0530

    VT-17 fix changes

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -2,12 +2,11 @@ class RolesController < ApplicationController
 
   def home_page
     #FIXME : create dynamic method for super_admin?
-    # if current_user.super_admin?
-    #   redirect_to companies_path
-    # #FIXME : create dynamic method for account_owner?
-    # elsif current_user.account_owner? || current_user.account_admin?
-    #   redirect_to users_path
-    # end
-    render html: "<strong>Not Found</strong>".html_safe
+    if current_user.super_admin?
+      redirect_to companies_path
+    #FIXME : create dynamic method for account_owner?
+    elsif current_user.account_owner? || current_user.account_admin?
+      redirect_to users_path
+    end
   end
 end
