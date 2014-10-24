9514faab0533cda795f4c2c2acf83062914c1784
siddharth@vinsol.com <siddharth@vinsol.com>
2014-10-24 22:12:33 +0530

    temp commit wip

--- app/models/exercise_task.rb
+++ app/models/exercise_task.rb
@@ -2,7 +2,7 @@ class ExerciseTask < ActiveRecord::Base
   acts_as :task
 
   has_attached_file :sample_solution
-  belongs_to :reveiwer, class_name: User
+  belongs_to :reviewer, class_name: User
   has_many :solutions
 
   validates :reviewer, presence: true, if: :reviewer_id?
