d80689099c0ea9e2b5743e635e2e894a779fcd6a
siddharth@vinsol.com <siddharth@vinsol.com>
2014-10-27 16:32:38 +0530

    remove assets precompile in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   config.assets.initialize_on_precompile = true
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
+  # config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
 
   # Generate digests for assets URLs.
   config.assets.digest = true
