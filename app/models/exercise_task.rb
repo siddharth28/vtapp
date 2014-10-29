4d9de2c545cabc2aeb5a970b15cd41a2399ccc9c
siddharth@vinsol.com <siddharth@vinsol.com>
2014-10-30 03:22:09 +0530

    add validation for file type and make task association dependent nullify

--- app/models/exercise_task.rb
+++ app/models/exercise_task.rb
@@ -1,9 +1,11 @@
 class ExerciseTask < ActiveRecord::Base
-  acts_as :task
+  acts_as :task, dependent: :nullify
 
   has_attached_file :sample_solution
   belongs_to :reviewer, class_name: User
   has_many :solutions
+
+  validates_attachment_file_name :sample_solution, :matches => [/zip\Z/, /rar\Z/]
 
   validates :reviewer, presence: true, if: :reviewer_id?
 end
