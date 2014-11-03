64a0a0c3a22d9a7e7258145403a49b4d6f798bd5
tanmay3011 <tanmay3011@gmail.com>
2014-11-03 17:04:48 +0530

    Fix autoSuggest

--- app/assets/javascripts/autoSuggest.js
+++ app/assets/javascripts/autoSuggest.js
@@ -2,11 +2,4 @@ $(document).ready(function () {
   $user_mentor = $('#user_mentor_name');
   $user_mentor_id = $('#user_mentor_id');
   checkAutoSuggest($user_mentor, $user_mentor_id);
-
-  $('#edit_track_1').on('submit', function() {
-    debugger;
-    if() {
-      e.preventDefault();
-    }
-  });
 });
