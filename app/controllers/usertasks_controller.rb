fddf7a27d723236a1e0b1d3652ba844af44fe48a
tanmay3011 <tanmay3011@gmail.com>
2014-11-10 14:44:15 +0530

    Fix task submission error

--- app/controllers/usertasks_controller.rb
+++ app/controllers/usertasks_controller.rb
@@ -6,8 +6,14 @@ class UsertasksController < ResourceController
   end
 
   def submit_task
-    @usertask.submit_task(params[:usertask])
-    redirect_to action: :task_description, id: @usertask
+    if params[:usertask][:url].blank? && params[:usertask][:comment].blank?
+      @usertask.errors[:url] << 'Either url or comment needs to be present for submission'
+      @usertask.errors[:comment] << 'Either url or comment needs to be present for submission'
+      render :task_description
+    else
+      @usertask.submit_task(params[:usertask])
+      redirect_to action: :task_description, id: @usertask
+    end
   end
 
   def task_description
