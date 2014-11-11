1801068ebc11e82c77da98129b0fc3b2e5a487fd
tanmay3011 <tanmay3011@gmail.com>
2014-11-12 00:10:33 +0530

    Fix specs

---
+++ spec/models/url_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe Urls do
+  describe 'belongs_to' do
+    it { should belong_to(:usertask) }
+  end
+
+  describe 'validation' do
+    it { should validate_presence_of(:usertask) }
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name).scoped_to(:usertask_id).case_insensitive }
+  end
+
+  describe 'add_submission_comment' do
+  end
+end
