5634d35b3c6ed64f663cdf2a79a9ea5bc7df307e
tanmay3011 <tanmay3011@gmail.com>
2014-11-12 11:40:06 +0530

    Fix track owner bug

--- app/models/track.rb
+++ app/models/track.rb
@@ -5,8 +5,8 @@ class Track < ActiveRecord::Base
   resourcify
 
   belongs_to :company
-  has_one :owner_role, -> { where(roles: { name: Track::ROLES[:track_owner], resource_type: 'Track' }) }, class_name: 'Role', foreign_key: :resource_id
   has_one :owner, through: :owner_role, source: :users
+  has_one :owner_role, -> { where(roles: { name: Track::ROLES[:track_owner], resource_type: 'Track' }) }, class_name: 'Role', foreign_key: :resource_id
   has_many :tasks, dependent: :destroy
   has_many :reviewer_role, -> { where(roles: { name: Track::ROLES[:track_reviewer], resource_type: 'Track' }) }, class_name: 'Role', foreign_key: :resource_id
   has_many :reviewers, through: :reviewer_role, source: :users
@@ -16,7 +16,6 @@ class Track < ActiveRecord::Base
   validates :company, :references, :description, :instructions, presence: true
   validates :name, uniqueness: { scope: :company_id, case_sensitive: false }, presence: true, length: { maximum: 255 }
   validates :owner_id, presence: true, on: :update
-
 
   attr_accessor :owner_id, :owner_name, :reviewer_id, :reviewer_name
 
@@ -47,11 +46,8 @@ class Track < ActiveRecord::Base
     # Not Fixed
     # FIXED
     # FIXME : This code can be simplified
-    user = find_user(owner_id)
-    user = unless user
-      company.owner
-    end
-    user.try(:add_role, ROLES[:track_owner], self)
+    user = find_user(owner_id) || company.owner
+    user.add_role(ROLES[:track_owner], self)
   end
 
   def replace_owner(owner_id)
